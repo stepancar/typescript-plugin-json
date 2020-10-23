51ebff5d90a393469813938637e80d5d10a223c1
stepancar <stepancar@hotmail.com>
2020-10-23 16:08:11 +0300

    feat(*): draft

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import fs from 'fs';
+import path from 'path';
+import tsModule from 'typescript/lib/tsserverlibrary';
+
+function isJson(fileName: string) {
+    return fileName.endsWith('json');
+}
+
+function getDtsSnapshot() {
+    // generate string 
+    // use typescript to convert it to real module
+}
+
+function init({ typescript: ts }: { typescript: typeof tsModule }) {
+  function create(info: ts.server.PluginCreateInfo) {
+    const directory = info.project.getCurrentDirectory();
+    const compilerOptions = info.project.getCompilerOptions();
+
+    // TypeScript plugins have a `cwd` of `/`, which causes issues with import resolution.
+    process.chdir(directory);
+    const _createLanguageServiceSourceFile = ts.createLanguageServiceSourceFile;
+    ts.createLanguageServiceSourceFile = (
+      fileName,
+      scriptSnapshot,
+      ...rest
+    ): ts.SourceFile => {
+      if (fileName.endsWith('json')) {
+        scriptSnapshot = getDtsSnapshot(
+          ts,
+          fileName,
+          scriptSnapshot,
+          compilerOptions,
+        );
+      }
+      const sourceFile = _createLanguageServiceSourceFile(
+        fileName,
+        scriptSnapshot,
+        ...rest,
+      );
+
+      return sourceFile;
+    };
+
+    // Updates virtual source files as files update.
+    const _updateLanguageServiceSourceFile = ts.updateLanguageServiceSourceFile;
+    ts.updateLanguageServiceSourceFile = (
+      sourceFile,
+      scriptSnapshot,
+      ...rest
+    ): ts.SourceFile => {
+      if (sourceFile.fileName.endsWith('json')) {
+        scriptSnapshot = getDtsSnapshot(
+          ts,
+          sourceFile.fileName,
+          scriptSnapshot,
+          compilerOptions,
+        );
+      }
+      sourceFile = _updateLanguageServiceSourceFile(
+        sourceFile,
+        scriptSnapshot,
+        ...rest,
+      );
+      if (sourceFile.fileName.endsWith('json')) {
+        sourceFile.isDeclarationFile = true;
+      }
+      return sourceFile;
+    };
+
+    return info.languageService;
+  }
+
+  function getExternalFiles(project: tsModule.server.ConfiguredProject) {
+    return project.getFileNames().filter(isJson);
+  }
+
+  return { create, getExternalFiles };
+}
+
+export = init;
